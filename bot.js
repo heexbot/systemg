8ece5ad9642e454debbe4c4f0c22e98113d6aed5
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-11-06 22:23:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1631,6 +1631,7 @@ welcomer.sendFile(canvas.toBuffer())
       });                    
 });
 
+
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
