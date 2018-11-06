85c6416425e9c5378bcf93de1d76ffaa5566597d
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-11-06 22:34:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1565,12 +1565,13 @@ welcomer.sendFile(canvas.toBuffer())
 })
       });                    
 });
+
+
+
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
 }
-
-
 client.on("ready", () => {
     var guild;
     while (!guild)
