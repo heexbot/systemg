ae6a8ea5db9d6b0825e94b08ab91f61a61d038bd
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-11-22 02:15:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,32 @@ if (message.content.startsWith(adminprefix + 'st')) {
 
 
 
-
+client.on('message', function(message) {
+    if(!message.channel.guild) return;
+    if(message.content === 'colors create') {
+    if(message.member.hasPermission('MANAGE_ROLES')) {
+    setInterval(function(){})
+    message.channel.send('يتم انشاء 200 لون انتضر | ▶️')
+    }else{
+    message.channel.send('ما معاك البرمشن المطلوب |❌🚫')
+    }
+    }
+    });
+    
+    client.on('message', message=>{
+    if (message.content === 'colors create'){
+    if(!message.channel.guild) return;
+    if (message.member.hasPermission('MANAGE_ROLES')){
+    setInterval(function(){})
+    let count = 0;
+    let ecount = 0;
+    for(let x = 1; x < 200; x++){
+    message.guild.createRole({name:x,
+    color: 'RANDOM'})
+    }
+    }
+    }
+    });
 
 
 
